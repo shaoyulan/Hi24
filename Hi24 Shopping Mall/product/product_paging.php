61d78db8665a47b5769cef075bd5f6a2555242cc
shaoyulan <45042818+shaoyulan@users.noreply.github.com>
2018-12-12 10:36:59 +0800

    move to new reop

---
+++ Hi24 Shopping Mall/product/product_paging.php
@@ -0,0 +1,63 @@
+<div class="product_paging">
+    
+        <!-- Header Carousel -->
+            <header class="page-title-pic"> 
+                <!-- Image Title -->
+              <img class="img-responsive" src="../images/all_img/page_title01.jpg" alt="">
+        <!-- Page Content 中間區塊-->
+        <div class="container">
+        <!-- Page/Breadcrumbs 麵包屑 (路徑)-->
+            <div class="row">
+                <div class="col-lg-12">             
+                    <ol class="breadcrumb">
+                        <li><a href="../index.html">Home</a>
+                        </li>
+                        <li><a href="product.html">WOMEN</a></li>
+                        <li class="active">上身類</li>
+                    </ol>
+                </div>
+            </div>
+            <!-- /.row -->
+            
+          <!-- 產品目錄/WOMEN -->
+                <div class="row service-two">
+  
+            </div><!-- /.產品目錄/WOMEN_row -->
+            
+            <hr>
+
+            <!-- 頁碼 -->
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#">&laquo;</a>
+                        </li>
+                        <li class="active">
+                            <a href="#">1</a>
+                        </li>
+                        <li>
+                            <a href="#">2</a>
+                        </li>
+                        <li>
+                            <a href="#">3</a>
+                        </li>
+                        <li>
+                            <a href="#">4</a>
+                        </li>
+                        <li>
+                            <a href="#">5</a>
+                        </li>
+                        <li>
+                            <a href="#">&raquo;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.頁碼row -->
+     </div>
+    <!-- /.中間區塊container -->
+
+    <!-- 回到最上TOP方 -->
+    <div id="scroll-top" style="display: block;"><a href="#"><i class="fa fa-angle-up"></i></a></div>
+</div> <!-- end-of-product-paging -->
