1032912f5af694cb3510c63e51333bcb721c74ac
shaoyu <poterpoter2@gmail.com>
2018-12-18 23:37:24 +0800

    DB connection updates

--- DB.php
+++ DB.php
@@ -1,19 +1,19 @@
 <?php 
 
-$in = [
-	'ht'=>'localhost',
-	'dn'=>'jinproject',
-	'pt'=>'3306',
-	'ct'=>'UTF8',
-	'un'=>'project',
-	'pd'=>'jinproject',
-];
-
 // $in = [
 // 	'ht'=>'localhost',
-// 	'dn'=>'hi24',
-// 	'pt'=>'8889',
+// 	'dn'=>'jinproject',
+// 	'pt'=>'3306',
 // 	'ct'=>'UTF8',
-// 	'un'=>'root',
-// 	'pd'=>'root',
+// 	'un'=>'project',
+// 	'pd'=>'jinproject',
 // ];
+
+$in = [
+	'ht'=>'localhost',
+	'dn'=>'hi24',
+	'pt'=>'8889',
+	'ct'=>'UTF8',
+	'un'=>'root',
+	'pd'=>'root',
+];
